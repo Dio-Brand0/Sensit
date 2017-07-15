a41ea5de0aac31adace6c3b6927e01fbd151b0d9
proi <gaviolafr@gmail.com>
2017-07-16 01:39:16 +0800

    3 charts are dsiplayed with dummy values

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,21 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app';
 
-  // lineChart
-  public lineChartData:Array<any> = [
-    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
-    //{data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'},
-    //{data: [18, 48, 77, 9, 100, 27, 40], label: 'Series C'}
+  // lineChart for thermal movement
+  public lineChartData1:Array<any> = [
+    {data: [65, 59, 80, 0, 0, 0, 0], label: 'Thermal Movement'},
   ];
+
+  // lineChart moisture movement
+  public lineChartData2:Array<any> = [
+    {data: [12, 44, 50, 0, 0, 0, 0], label: 'Moisture Movement'},
+  ];
+
+  // lineChart for soil movement
+  public lineChartData3:Array<any> = [
+    {data: [22, 5, 10, 0, 0, 0, 0], label: 'soil movement'},
+  ];
+
   public lineChartLabels:Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
   public lineChartOptions:any = {
     responsive: true
@@ -27,41 +36,41 @@ export class AppComponent {
       pointHoverBackgroundColor: '#fff',
       pointHoverBorderColor: 'rgba(148,159,177,0.8)'
     },
-    { // dark grey
-      backgroundColor: 'rgba(77,83,96,0.2)',
-      borderColor: 'rgba(77,83,96,1)',
-      pointBackgroundColor: 'rgba(77,83,96,1)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(77,83,96,1)'
-    },
-    { // grey
-      backgroundColor: 'rgba(148,159,177,0.2)',
-      borderColor: 'rgba(148,159,177,1)',
-      pointBackgroundColor: 'rgba(148,159,177,1)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
-    }
+    // { // dark grey
+    //   backgroundColor: 'rgba(77,83,96,0.2)',
+    //   borderColor: 'rgba(77,83,96,1)',
+    //   pointBackgroundColor: 'rgba(77,83,96,1)',
+    //   pointBorderColor: '#fff',
+    //   pointHoverBackgroundColor: '#fff',
+    //   pointHoverBorderColor: 'rgba(77,83,96,1)'
+    // },
+    // { // grey
+    //   backgroundColor: 'rgba(148,159,177,0.2)',
+    //   borderColor: 'rgba(148,159,177,1)',
+    //   pointBackgroundColor: 'rgba(148,159,177,1)',
+    //   pointBorderColor: '#fff',
+    //   pointHoverBackgroundColor: '#fff',
+    //   pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+    // }
   ];
   public lineChartLegend:boolean = true;
   public lineChartType:string = 'line';
  
-  public randomize():void {
-    let _lineChartData:Array<any> = new Array(this.lineChartData.length);
-    for (let i = 0; i < this.lineChartData.length; i++) {
-      _lineChartData[i] = {data: new Array(this.lineChartData[i].data.length), label: this.lineChartData[i].label};
-      for (let j = 0; j < this.lineChartData[i].data.length; j++) {
-        _lineChartData[i].data[j] = Math.floor((Math.random() * 100) + 1);
-      }
-    }
-    this.lineChartData = _lineChartData;
-  }
+  // public randomize():void {
+  //   let _lineChartData:Array<any> = new Array(this.lineChartData.length);
+  //   for (let i = 0; i < this.lineChartData.length; i++) {
+  //     _lineChartData[i] = {data: new Array(this.lineChartData[i].data.length), label: this.lineChartData[i].label};
+  //     for (let j = 0; j < this.lineChartData[i].data.length; j++) {
+  //       _lineChartData[i].data[j] = Math.floor((Math.random() * 100) + 1);
+  //     }
+  //   }
+  //   this.lineChartData = _lineChartData;
+  // }
  
   // events
-  public chartClicked(e:any):void {
-    console.log(e);
-  }
+  // public chartClicked(e:any):void {
+  //   console.log(e);
+  // }
  
   public chartHovered(e:any):void {
     console.log(e);
