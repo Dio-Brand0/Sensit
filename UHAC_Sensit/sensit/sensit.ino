4ec9b8bd520661a6ffbc2f41c621159b2965cfb6
Gillian Claire Cancio <Gillian Claire Cancio>
2017-07-16 10:09:53 +0800

    pnakafinal for the win

--- UHAC_Sensit/sensit/sensit.ino
+++ UHAC_Sensit/sensit/sensit.ino
@@ -3,15 +3,10 @@
 #include <EthernetClient.h>
 #include <EthernetServer.h>
 
-  byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
-  float a[] = {0,0,0};
-  IPAddress ip(169,254,90,73);
-  EthernetClient client;
-  
+float a[] = {0,0,0};
+
 void setup() {
   // put your setup code here, to run once:
-  Ethernet.begin(mac, ip);
-  delay(1000);
   Serial.begin(9600);
   pinMode(0, INPUT);
   pinMode(1, INPUT);
@@ -25,10 +20,10 @@ void loop() {
     a[i] = analogRead(i);
     a[i] = a[i] / 1023 * 5;
   }
-  Serial.print( a[0] );
- // Serial.print(" ");
- //Serial.print(a[1] );
- // Serial.print(" ");
- //Serial.println(a[2]);
+  Serial.print(a[0]);
+  Serial.print(" ");
+  Serial.print(a[1]);
+  Serial.print(" ");
+  Serial.println(a[2]);
   delay(3000);
 }
--- UHAC_Sensit/toFirebase.py
+++ UHAC_Sensit/toFirebase.py
@@ -13,22 +13,29 @@ print firebase_url
 fixed_interval = 3
 while 1:
 	try:          
-		sensor_thermal = ser.readline()
+		string = ser.readline()
+		#sensor_thermal = ser.readline()
     	#sensor_moisture = ser.readline()
     	#sensor_soil = ser.readline()
+    		sensor_thermal = string[0:2]
+    		sensor_moisture = string[3:5]
+    		sensor_soil = string[6:8]
 
     	#current time and date
   		time_hhmmss = time.strftime('%H:%M:%S')
 		date_mmddyyyy = time.strftime('%d/%m/%Y')
     
-		#print sensor_thermal + ',' + time_hhmmss + ',' + date_mmddyyyy
-		print time_hhmmss
-		print date_mmddyyyy
+		print sensor_thermal + ',' + time_hhmmss + ',' + date_mmddyyyy
+		#print time_hhmmss
+		#print date_mmddyyyy
     	#insert record
     	#data = {'date':date_mmddyyyy,'time':time_hhmmss,'value':sensor_thermal}
-		#result = requests.post(firebase_url + '/'+ '/temperature.json', data=json.dumps(data))
-    
-		#print 'Record inserted. Result Code = ' + str(result.status_code) + ',' + result.text
+		result = requests.post(firebase_url + '/'+ '/temperature.json', data=json.dumps({'date':date_mmddyyyy,'time':time_hhmmss,'value':sensor_thermal}))
+		print 'Record inserted. Result Code = ' + str(result.status_code) + ',' + result.text
+		result = requests.post(firebase_url + '/'+ '/moisture.json', data=json.dumps({'date':date_mmddyyyy,'time':time_hhmmss,'value':sensor_moisture}))
+		print 'Record inserted. Result Code = ' + str(result.status_code) + ',' + result.text
+		result = requests.post(firebase_url + '/'+ '/soil.json', data=json.dumps({'date':date_mmddyyyy,'time':time_hhmmss,'value':sensor_soil}))
+		print 'Record inserted. Result Code = ' + str(result.status_code) + ',' + result.text
 		time.sleep(fixed_interval)
 	except IOError:
 		print('Error! Something went wrong.')
